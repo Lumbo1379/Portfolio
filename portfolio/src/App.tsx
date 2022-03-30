64d746d46d95f4e55f78555773b8909e764f43c3
Callum Thompson <36245864+Lumbo1379@users.noreply.github.com>
2022-03-30 23:34:41 +0100

    Add eslint. Fix linting errors and warnings

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,13 +1,12 @@
-import { Fragment } from 'react';
+import { ReactElement } from 'react';
 import Headline from './components/Headline';
 import './App.css';
 
-function App() {
-  return (
-    <Fragment>
-      <Headline content="This is my headline" keywords={{ "is": "#FF0000", "headline": "#0000FF" }} />
-    </Fragment>
-  );
-}
+const App = (): ReactElement => (
+    <Headline
+        content="This is my headline"
+        keywords={{ is: '#FF0000', headline: '#0000FF' }}
+    />
+);
 
 export default App;
--- portfolio/src/setupTests.ts
+++ portfolio/src/setupTests.ts
@@ -1,4 +1,5 @@
 import Enzyme from 'enzyme';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new Adapter() });
